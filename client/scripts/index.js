8db238eb556f56d0a37d86d579be8910528c58d5
Viengo <viengooleg@gmail.com>
2015-05-16 21:18:27 +0400

    add slow scroll script

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,5 +1,16 @@
+
+//Slider Settings
 jQuery(document).ready(function ($) {
     $(".royalSlider").royalSlider({
         keyboardNavEnabled: true
+    });
+
+//Slow scroll
+
+    $('a[href^="#"]').click(function(){
+        if(document.getElementById($(this).attr('href').substr(1)) != null) {
+            $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500);
+        }
+        return false;
     });
 });
